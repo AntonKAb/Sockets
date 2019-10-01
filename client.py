760615c9d215bd6b36fcd4341d33bc599908d2db
“AntonKAb” <anton_10_1999@mail.ru>
2019-10-01 14:20:13 +0700

    Client update.

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import socket
 
+_list = ['hour', 'seconds', 'minutes', 'stop']
 _input = input('Enter request(hour, seconds, minutes, stop): ')
+
+while _input.lower() not in _list:
+    _input = input('Enter request(hour, seconds, minutes, stop): ')
 
 with socket.create_connection(('127.0.0.1', 10001)) as sock:
     sock.settimeout(2)
